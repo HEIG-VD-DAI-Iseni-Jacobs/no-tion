8a7670d7115d02c5bf57aafa6885cc5eeb0c4bad
aladin-heig <aladin.iseni@hes-so.ch>
2024-12-05 08:42:18 +0100

    utils

--- src/main/java/ch/heigvd/dai/utils/MainUtils.java
+++ src/main/java/ch/heigvd/dai/utils/MainUtils.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainUtils {
-    public static String[] parseInput(String input) {
-        List<String> tokens = new ArrayList<>();
-        boolean inQuotes = false;
-        StringBuilder sb = new StringBuilder();
+  public static String[] parseInput(String input) {
+    List<String> tokens = new ArrayList<>();
+    boolean inQuotes = false;
+    StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
+    for (int i = 0; i < input.length(); i++) {
+      char c = input.charAt(i);
 
-            if (c == '"') {
-                inQuotes = !inQuotes;
-            } else if (c == ' ' && !inQuotes) {
-                if (!sb.isEmpty()) {
-                    tokens.add(sb.toString());
-                    sb.setLength(0);
-                }
-            } else {
-                sb.append(c);
-            }
-        }
-
+      if (c == '"') {
+        inQuotes = !inQuotes;
+      } else if (c == ' ' && !inQuotes) {
         if (!sb.isEmpty()) {
-            tokens.add(sb.toString());
+          tokens.add(sb.toString());
+          sb.setLength(0);
         }
-
-        return tokens.toArray(new String[0]);
+      } else {
+        sb.append(c);
+      }
     }
+
+    if (!sb.isEmpty()) {
+      tokens.add(sb.toString());
+    }
+
+    return tokens.toArray(new String[0]);
+  }
 }
