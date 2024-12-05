98c342d7c19dc38d0d473379ee1d806359487b92
aladin-heig <aladin.iseni@hes-so.ch>
2024-12-05 09:28:16 +0100

    models

---
+++ src/main/java/ch/heigvd/dai/model/User.java
@@ -0,0 +1,48 @@
+package ch.heigvd.dai.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class User {
+  private String name;
+  private List<Note> notes;
+
+  public User(String name) {
+    this.name = name;
+    this.notes = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<Note> getNotes() {
+    return notes;
+  }
+
+  public boolean hasNoteWithTitle(String title) {
+    for (Note note : notes) {
+      if (note.getTitle().equals(title)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void addNote(Note note) {
+    notes.add(note);
+  }
+
+  public boolean deleteNoteByTitle(String title) {
+    Iterator<Note> iterator = notes.iterator();
+    while (iterator.hasNext()) {
+      Note note = iterator.next();
+      if (note.getTitle().equals(title)) {
+        iterator.remove();
+        return true;
+      }
+    }
+    return false;
+  }
+}
